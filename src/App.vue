c7f76a32f3d83c854a5018862286c53e39f0b876
Steven Bisbikos <steven.bisbikos@tcgplayer.com>
2021-08-28 00:19:10 -0400

    Components
    Created some component shells + preliminary scaffolding for page + hello world removal.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,57 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="user-manager">
+    <header class="user-manager__header">
+      <app-header />
+    </header>
+    <nav class="user-manager__nav">
+      <side-nav />
+    </nav>
+    <section class="user-manager__content">
+      <h1>User Management</h1>
+      <div class="user-manager__toolbar">
+      <!-- add goes here -->
+      </div>
+      <div class="user-manager__column-titles">
+        <div>Title 1</div>
+        <div>Title 1</div>
+        <div>Title 1</div>
+        <div>Title 1</div>
+        <div>Title 1</div>
+      </div>
+      <!-- loading flag -->
+      <!-- transition group -->
+      <!--  <user v-for="(user, index) in users" :key="index" :user-info="user" index="index" /> user sends emitter to delete  -->
+    </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import AppHeader from '@/components/AppHeader';
+import SideNav from '@/components/SideNav';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    AppHeader,
+    SideNav,
   },
 };
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Roboto', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.user-manager {
+  display: grid;
+  grid-template-areas: 
+  "header header"
+  "side-nav content";
 }
 </style>

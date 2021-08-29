7157395b8485aa5156a25c315aed70a9c3fcb883
Steven Bisbikos <steven.bisbikos@tcgplayer.com>
2021-08-29 12:36:03 -0400

    Deps and State
    ... included vuelidate and current Store for managing users centrally.

--- src/main.js
+++ src/main.js
@@ -1,16 +1,20 @@
 import Vue from 'vue';
 import Buefy from 'buefy';
+import Vuelidate from 'vuelidate';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import {
-  faHome, faThLarge, faChartLine, faUsers, faEdit,
+  faHome, faThLarge, faChartLine, faUsers, faEdit, faTimes,
 } from '@fortawesome/free-solid-svg-icons';
 import App from './App';
 import store from './store';
 import 'buefy/dist/buefy.css';
 
-library.add(faHome, faThLarge, faChartLine, faUsers, faEdit);
+library.add(faHome, faThLarge, faChartLine, faUsers, faEdit, faTimes);
 Vue.component('font-awesome-icon', FontAwesomeIcon);
+
+// Register Vuelidate for validation
+Vue.use(Vuelidate);
 
 Vue.use(Buefy);
 Vue.config.productionTip = false;
--- src/store/index.js
+++ src/store/index.js
@@ -4,12 +4,13 @@ import userApi from './apis/user.api';
 
 Vue.use(Vuex);
 
-const GET_USERS = 'GET_USERS';
+const SET_USERS = 'SET_USERS';
 const SET_IS_LOADING = 'SET_IS_LOADING';
+const PROCESS_USER = 'PROCESS_USER';
 
 export default new Vuex.Store({
   state: {
-    users: {},
+    users: [],
     isLoading: false,
   },
   actions: {
@@ -17,18 +18,32 @@ export default new Vuex.Store({
       commit(SET_IS_LOADING, true);
       userApi.getUsers().then((response) => {
         setTimeout(() => {
-          commit(GET_USERS, response.data);
+          commit(SET_USERS, response.data);
           commit(SET_IS_LOADING, false);
         }, 1000);
       });
     },
+    processUser({ commit }, user) {
+      commit(PROCESS_USER, user);
+    },
   },
   mutations: {
-    [GET_USERS]: (state, users) => {
+    [SET_USERS]: (state, users) => {
       state.users = users;
     },
     [SET_IS_LOADING]: (state, isLoading) => {
       state.isLoading = isLoading;
+    },
+    [PROCESS_USER]: (state, user) => {
+      debugger;
+      const userRecord = state.users.filter((u) => u.id === user.id).find((r) => r);
+      const indexToUpdate = userRecord ? state.users.indexOf(userRecord) : -1;
+
+      if (indexToUpdate > -1) {
+        state.users[indexToUpdate] = user;
+      } else {
+        state.users.push(user);
+      }
     },
   },
 });

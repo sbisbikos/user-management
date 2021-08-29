45c62ebb99093d97b63d0933e77a949fee6d588f
Steven Bisbikos <steven.bisbikos@tcgplayer.com>
2021-08-29 00:15:23 -0400

    SVG and Mock Data Changes
    ... added FontAwesome and Icon component loading + json data changes + mock logo.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
 import Vue from 'vue';
-import App from './App.vue';
+import Buefy from 'buefy';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+  faHome, faThLarge, faChartLine, faUsers, faEdit,
+} from '@fortawesome/free-solid-svg-icons';
+import App from './App';
 import store from './store';
+import 'buefy/dist/buefy.css';
 
+library.add(faHome, faThLarge, faChartLine, faUsers, faEdit);
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+
+Vue.use(Buefy);
 Vue.config.productionTip = false;
 
 new Vue({
